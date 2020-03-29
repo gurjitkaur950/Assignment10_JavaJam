87993e640cd2e8e770e16688fbe14023a6dd5bad
gurjitkaur950 <saigurjit011195@gmail.com>
2020-03-29 14:14:54 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>JavaJam Coffee House</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="javajam.css">
+</head>
+<body>
+<div id="wrapper">
+<header>
+<h1>JavaJam Coffee House</h1>
+</header>
+<nav>
+<a href="index.php">Home</a> &nbsp; 
+<a href="menu.html">Menu</a> &nbsp; 
+<a href="music.html">Music</a> &nbsp; 
+<a href="jobs.html">Jobs</a>
+</nav>
+    <hr/>
+<main>
+<img src="1.jpg"  align="right" width="400" height="300" alt="The Winding Road">
+<h2>Follow the Winding Road to JavaJam</h2>
+We&#39;re a little out of the way, but take a drive down Route 42 to JavaJam today! Indulge in our locally roasted free-trade coffee and home-made pastries. You&#39;ll feel right at home at JavaJam!
+<h3>JavaJam Coffee House features:</h3>
+<ul>
+  <li>Specialty Coffee and Tea</li>
+  <li>Bagels, Muffins, and Organic Snacks</li>
+  <li>Music and Poetry Readings</li>
+  <li>Open Mic Night</li>
+</ul>
+<div>
+54321 Route 42<br>
+Ellison Bay, WI 54210<br>
+888-555-5555<br><br>
+</div>
+</main>
+<footer>
+Copyright &copy; 2016 JavaJam Coffee House<br>
+<a href="mailto:yourfirstname@yourlastname.com">yourfirstname@yourlastname.com</a>
+</footer>
+</div>
+</body>
+</html>
